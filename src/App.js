f317d06da76e4d2a083a3b99566fb1c38d756ea5
Asamu victor <asamuvictor03@gmail.com>
2022-09-15 22:07:30 +0100

    started working signup page interface

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Route, Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Signup from './components/Signup';
 
 function App() {
   return (
     <>
-      <Router>
+      <Routes>
         <Route  path='/' element={<Signup/>}/>
-      </Router>
+      </Routes>
     </>
   );
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,19 @@
 import React from 'react'
-
+import "../styles/Signup.css"
+import icon1 from "../images/Dribbble (3)_1.jpg"
 const Signup = () => {
   return (
     <>
-      
+      <section className='section'>
+      <div class="first-half-body">
+            <div class="product-logo">
+                my<span class="logo-item">N</span>ote
+              </div>
+              <img src={icon1} alt="" width="400" class="central-img"/>
+        </div>
+      </section>
     </>
   )
 }
 
-export default Signup
+export default Signup;
