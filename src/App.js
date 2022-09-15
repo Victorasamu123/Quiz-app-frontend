a0e4c95fc3d2a43d8abb6403201240c6c18d301a
Asamu victor <asamuvictor03@gmail.com>
2022-09-15 21:55:17 +0100

    started working on signup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Router } from 'react-router-dom';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <>
-      
+      <Router>
+        <Route  path='/' element={<Signup/>}/>
+      </Router>
     </>
   );
 }
