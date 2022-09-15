791439151ccffe4368db4df9b5d2bc23a835b7f8
Asamu victor <asamuvictor03@gmail.com>
2022-09-15 22:21:00 +0100

    complleted signup page interface

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Signup from './components/Signup';
+import Signin from './components/Signin';
 
 function App() {
   return (
     <>
       <Routes>
         <Route  path='/' element={<Signup/>}/>
+        <Route  path='/signin' element={<Signin/>}/>
       </Routes>
     </>
   );
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,15 +1,36 @@
 import React from 'react'
 import "../styles/Signup.css"
 import icon1 from "../images/Dribbble (3)_1.jpg"
+import { Link } from 'react-router-dom'
 const Signup = () => {
   return (
     <>
       <section className='section'>
       <div class="first-half-body">
             <div class="product-logo">
-                my<span class="logo-item">N</span>ote
+                my<span class="logo-item">Q</span>uiz
               </div>
               <img src={icon1} alt="" width="400" class="central-img"/>
+        </div>
+        <div class="second-half-body">
+            <h2>Welcomme to myQuiz</h2>
+            <div class="fullname-input">
+            <label for="Fullname">Fullname</label> <br/>
+            <input type="text" placeholder="Enter your Fullname" id="fullname"/>
+            </div>
+            <div class="email-input">
+            <label for="email">Email Address</label> <br/>
+            <input type="email" placeholder="Enter email Address" id="emailadd"/>
+            </div>
+            <div class="password-input">
+            <label for="password">Password</label> <br/>
+            <input type="password" placeholder="Enter password at least 8 character" id="password"/>
+            </div>
+            <div id="warning"></div>
+            <button class="sign-in-button" onclick="signUpPage()">sign in</button>
+            <div class="confirmation">
+                Already have an account?<Link to="/signin">Log in</Link>
+            </div>
         </div>
       </section>
     </>
