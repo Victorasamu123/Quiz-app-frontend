e29760ff02bdfd71d08339190050f79b83447ddb
Asamu victor <asamuvictor03@gmail.com>
2022-09-16 12:44:27 +0100

    completed signup validation

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,34 +1,57 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import axios from "axios"
 import "../styles/Signup.css"
 import icon1 from "../images/Dribbble (3)_1.jpg"
 import { Link } from 'react-router-dom'
 const Signup = () => {
+  const [firstname, setfirstname] = useState("");
+  const [lastname, setlastname] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [firstnameerror, setfirstnameerror] = useState("");
+  const [lastnameerror, setlastnameerror] = useState("");
+  const [emailerror, setemailerror] = useState("");
+  const [passworderror, setpassworderror] = useState("");
+  const signup =()=>{
+    let regexForFirstName=/^[\w]{2,}$/
+    let regexForLastName=/^[\w]{2,}$/
+    let regexForEmail=/^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/
+    let regexForPassword=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/
+  }
   return (
     <>
       <section className='section'>
-      <div class="first-half-body">
-            <div class="product-logo">
-                my<span class="logo-item">Q</span>uiz
+      <div className="first-half-body">
+            <div className="product-logo">
+                my<span className="logo-item">Q</span>uiz
               </div>
-              <img src={icon1} alt="" width="400" class="central-img"/>
+              <img src={icon1} alt="" width="400" className="central-img"/>
         </div>
-        <div class="second-half-body">
+        <div className="second-half-body">
             <h2>Welcomme to myQuiz</h2>
-            <div class="fullname-input">
-            <label for="Fullname">Fullname</label> <br/>
-            <input type="text" placeholder="Enter your Fullname" id="fullname"/>
+            <div className="fullname-input">
+            <label for="Fullname">Firstname</label> <br/>
+            <input type="text" placeholder="Enter your Fullname" id="fullname" onChange={(e)=>setfirstname(e.target.value)} value={firstname}/>
+            <div className='text-danger'>{firstnameerror}</div>
             </div>
-            <div class="email-input">
-            <label for="email">Email Address</label> <br/>
-            <input type="email" placeholder="Enter email Address" id="emailadd"/>
+            <div className="fullname-input mt-2">
+            <label for="Fullname">Lastname</label> <br/>
+            <input type="text" placeholder="Enter your Fullname" id="fullname" onChange={(e)=>setlastname(e.target.value)} value={lastname}/>
+            <div className='text-danger'>{lastnameerror}</div>
             </div>
-            <div class="password-input">
+            <div className="email-input">
+            <label for="email">Email  Address</label> <br/>
+            <input type="email" placeholder="Enter email Address" id="emailadd" onChange={(e)=>setemail(e.target.value)} value={email}/>
+            <div className='text-danger'>{emailerror}</div>
+            </div> 
+            <div className="password-input">
             <label for="password">Password</label> <br/>
-            <input type="password" placeholder="Enter password at least 8 character" id="password"/>
+            <input type="password" placeholder="Enter password at least 8 character" id="password" onChange={(e)=>setpassword(e.target.value)} value={password}/>
+            <div className='text-danger'>{passworderror}</div>
             </div>
             <div id="warning"></div>
-            <button class="sign-in-button">sign up</button>
-            <div class="confirmation">
+            <button  className="sign-in-button" onClick={signup}>sign up</button>
+            <div className="confirmation">
                 Already have an account?<Link to="/signin" className='link'>Log in</Link>
             </div>
         </div>
